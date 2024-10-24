0980b552a26abb3b5eeb22c3686d42612d87ac8d
Eduardo Gimenes <dugimenes@gmail.com>
2024-10-23 22:50:10 -0300

    Add CRUD comentarios/Postagens

---
+++ src/Blog.Api/Controllers/ComentarioController.cs
@@ -0,0 +1,85 @@
+﻿using Blog.Data.Models;
+using Blog.Web.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Blog.Api.Controllers
+{
+    [ApiController]
+    [Route("api/comentario")]
+    public class ComentarioController : ControllerBase
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+        readonly JwtSettings _jwtSettings;
+
+        public ComentarioController(SignInManager<ApplicationUser> signInManager, 
+                                    UserManager<ApplicationUser> userManager, 
+                                    IOptions<JwtSettings> jwtSettings, 
+                                    ApplicationDbContext context)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _context = context;
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Comentario>>> Obter()
+        {
+            return await _context.Comentarios.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Post>> Obter(int id)
+        {
+            var comentario = await _context.Posts.FindAsync(id);
+
+            return comentario;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Comentario>> CadastrarComentario(Comentario comentario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return ValidationProblem(new ValidationProblemDetails(ModelState)
+                {
+                    Title = "Erros de validação"
+                });
+            }
+
+            _context.Comentarios.Add(comentario);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Obter), new { id = comentario.Id }, comentario);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> AtualizarComentario(int id, Comentario comentario)
+        {
+            if (id != comentario.Id) return BadRequest();
+
+            if (!ModelState.IsValid) return ValidationProblem(ModelState);
+
+            _context.Comentarios.Update(comentario);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteComentario(int id)
+        {
+            var comentario = await _context.Comentarios.FindAsync(id);
+
+            _context.Comentarios.Remove(comentario);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ src/Blog.Api/Controllers/PostsController.cs
@@ -0,0 +1,86 @@
+﻿using Blog.Data.Models;
+using Blog.Web.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Blog.Api.Controllers
+{
+    [ApiController]
+    [Route("api/postagem")]
+    public class PostsController : ControllerBase
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+        readonly JwtSettings _jwtSettings;
+        
+
+        public PostsController(SignInManager<ApplicationUser> signInManager, 
+                                UserManager<ApplicationUser> userManager,
+                                IOptions<JwtSettings> jwtSettings, 
+                                ApplicationDbContext context)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _context = context;
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Post>>> Obter()
+        {
+            return await _context.Posts.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Post>> Obter(int id)
+        {
+            var post = await _context.Posts.FindAsync(id);
+
+            return post;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Post>> CadastrarPostagem(Post postagem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return ValidationProblem(new ValidationProblemDetails(ModelState)
+                {
+                    Title = "Erros de validação"
+                });
+            }
+
+            _context.Posts.Add(postagem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Obter), new { id = postagem.Id }, postagem);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> AtualizarPostagem(int id, Post postagem)
+        {
+            if (id != postagem.Id) return BadRequest();
+
+            if (!ModelState.IsValid) return ValidationProblem(ModelState);
+
+            _context.Posts.Update(postagem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeletePostagem(int id)
+        {
+            var postagem = await _context.Posts.FindAsync(id);
+
+            _context.Posts.Remove(postagem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
